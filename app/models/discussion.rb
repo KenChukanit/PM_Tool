c9b1e783f5452b97bd77ea63b7f06fb5b82e505d
KenChukanit <chonlasek.c@gmail.com>
2021-01-17 20:11:27 -0800

    fix bug on delete

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,6 @@
 class Discussion < ApplicationRecord
     belongs_to :project
-    has_many :comments
+    has_many :comments, dependent: :destroy
     belongs_to :user, optional: true
     validates :title, presence: {message: 'must be provided'}
     validates :description, presence: {message: 'must be provided'}
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-    has_many :tasks
-    has_many :discussions
+    has_many :tasks, dependent: :destroy
+    has_many :discussions, dependent: :destroy
     belongs_to :user, optional: true
     validates :title, presence: {message: 'must be provided'},uniqueness: true
 end
